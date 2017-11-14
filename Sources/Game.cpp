bc9778d703ce4c815841d8bcb8819db074b5a274
NaoyaKinoshita <hw16a063@oecu.jp>
2017-11-14 15:43:44 +0900

    ゲーム開始時にBGM が再生されるようにしました。 弾の発射時と障害物に当たった時にSEが再生されるようにしました。

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -7,8 +7,8 @@
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
-// TODO: PlayBGM()関数を使って、BGMを再生する。(G)
-// TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
+// TODO: PlayBGM()関数を使って、BGMを再生する。(G) HW16A063 木下直矢
+// TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H) HW16A063 木下直矢
 
 
 Vector2 cloudPos;       //!< 雲の位置
@@ -26,6 +26,10 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    
+// BGMの再生
+    PlayBGM("bgm_maoudamashii_8bit07.mp3");
+    
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -33,6 +37,8 @@ void Update()
 {
     // 弾の発射
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
+        PlaySound("se_maoudamashii_system20.mp3");      // SEの再生
+        
         bulletPos = cannonPos + Vector2(50, 10);
     }
 
@@ -43,6 +49,7 @@ void Update()
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
+            PlaySound("se_maoudamashii_explosion06.mp3");    // SEの再生
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
